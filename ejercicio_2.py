08999680da9f63f7998d9d66067cc39da6eddc1c
T0kyoB0y <119447307+T0kyoB0y@users.noreply.github.com>
2023-05-10 23:08:29 -0400

    Update ejercicio_2.py
    Segundo codigo comentado completamente

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,28 +1,31 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from ejercicio_1 import * 
-
-C1csv
-
-def average_row(array):
+from ejercicio_1 import *   #   Importamos todo del :ejercicio_1.py: 
+                            #   Es decir las librerias numpy y import numpy as npimport numpy as np
+    
+def average_row(array): #   Creamos una función, esta calculará la media aritmetica de las filas.
+    #   El resultado se almacenará en un diccionario, de momento se creará uno vacío llamado :dict_average_row:
     dict_average_row = {}
-    average_row = np.average(array,axis=1)
-    for row in range(16):
-        dict_average_row[row+1] = average_row[row]
-    return dict_average_row
+    average_row = np.average(array,axis=1)  #   Usamos numpy.average() con axis=0 para calcular el promedio de las filas
+    count_row, count_column = array.shape   #   Se calcula el numero de columnas y filas para el ciclo for de columnas y filas
+    
+    for row in range(count_row): #   El ciclo itera el numero de veces dependiendo de el numero de filas de la matriz
+        dict_average_row[row+1] = average_row[row]  #   Ahora el promedio se almacenará en el diccionario creado anteriormente
+        #   El diccionario almacenará {"key": value}
+        #   En donde la :key: es el numero de la columna
+        #   Y el :value: es el valor de la media aritmetica de esa columna
+    return dict_average_row  #   Retorna el valor del diccionario para que pueda ser usado
 
 def average_column(array):
-    dict_average_column = {}
-    result_reverse = np.transpose(array)
-    average_column = np.average(result_reverse,axis=1)
-    for row in range(16):
-        dict_average_column[row+1] = average_column[row]
-    return dict_average_column
+    dict_average_column = {}    #   El resultado se almacenará en un diccionario :dict_average_column:
+    average_column = np.average(result_reverse,axis=0)  #   Usamos numpy.average() con axis=1 para calcular el promedio de las filas
+    count_row, count_column = array.shape
+    
+    for column in range(count_column): #   El ciclo itera el numero de veces dependiendo de el numero de columnas de la matriz
+        dict_average_column[column+1] = average_column[column]  #   Lo mismo que la anterior función, se almacena el contenido en el diccionario
+    return dict_average_column  #   Retorna el valor del diccionario para que pueda ser usado
 
-
-dict_row = average_row(C1csv)
-dict_column = average_column(C1csv)
-print(dict_row)
+dict_row = average_row(C1csv)   #   Asignamos los valores de las filas a :dict_row:
+dict_column = average_column(C1csv)   #   Asignamos los valores de las columna a :dict_column:
+print(dict_row) #   Hacemos :print: del resultado
 print(dict_column)
 
 
