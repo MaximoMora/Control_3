2d4fc7554bdd727dd46f0ad9b28f8211a5f93d29
StillEffect6 <96755454+StillEffect6@users.noreply.github.com>
2023-05-10 23:17:58 -0400

    Update ejercicio_7.py
    comentary

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -1,16 +1,16 @@
-from ejercicio_2 import *
-from ejercicio_3 import *
-from ejercicio_4 import *
-from ejercicio_6 import *
-import matplotlib.pyplot as plt 
+from ejercicio_2 import * #importa todo del ejercicio 2
+from ejercicio_3 import * #importa todo del ejercicio 3
+from ejercicio_4 import * #importa todo del ejercicio 4
+from ejercicio_6 import * #importa todo del ejercicio 5
 
-C1C2_row = average_row(C1C2)
-C1C2_column = average_column(C1C2)
-C1C2_row_sort = ord_burbuja(C1C2_row)
-C1C2_column_sort = ord_burbuja(C1C2_column)
+C1C2_row = average_row(C1C2) #El promedio de las filas 
+C1C2_column = average_column(C1C2) #El promedio de las columnas
+C1C2_row_sort = ord_burbuja(C1C2_row) #El promedio ordenado de las filas
+C1C2_column_sort = ord_burbuja(C1C2_column) #El promedio ordenado de las columnas
 
 
-Show_Bar(C1C2_row.values(),"Filas")
-Show_Bar(C1C2_column.values(),"Columna")
-Show_Bar(C1C2_row_sort.values(),"Filas")
-Show_Bar(C1C2_column_sort.values(),"Columnas")
+Show_Bar(C1C2_row.values(),"Filas") #Grafico del promedio de las filas 
+Show_Bar(C1C2_column.values(),"Columna") #Grafico del promedio de las columnas 
+Show_Bar(C1C2_row_sort.values(),"Filas") #Grafico del promedio ordenado de las filas
+Show_Bar(C1C2_column_sort.values(),"Columnas") ##El promedio ordenado de las columnas
+
