2de36d677017ee4aa32501c5b86700af9c95e798
StillEffect6 <96755454+StillEffect6@users.noreply.github.com>
2023-05-10 21:36:51 -0400

    Update ejercicio_3.py
    se puede graficar

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -2,7 +2,22 @@ import matplotlib.pyplot as plt
 
 from ejercicio_2 import *
 
+dict_row
+dict_column
 
+def Show_Bar(TValue)
+    E = []
+    fig, ax = plt.subplots()
+    for i in range(0,16):
+        a = "Fila " + str(i+1) 
+        E.append(a)
+
+    p = ax.bar(E, TValue)
+    ax.bar_label(p, label_type='edge')
+    plt.xlabel("Columnas")
+    plt.ylabel("Valores")
+    plt.title("Promedios por Columnas")
+    plt.show()
 
 plt.bar()
 plt.show()
